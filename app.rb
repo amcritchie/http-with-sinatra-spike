e504f3106a83faba4a2d7a2f7d7345fae97641c6
Alex McRitchie <amcritchie@gmail.com>
2014-06-26 16:28:05 -0600

    commiting spike

--- app.rb
+++ app.rb
@@ -1,16 +1,39 @@
 require "sinatra/base"
-require "sinatra/reloader"
+# require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-  register Sinatra::Reloader
+  # register Sinatra::Reloader
 
   def initialize
     super
-    @items = []
+    @items = ['item 1', 'item 2', 'item 3']
   end
 
-  get "/" do
-    "items length: #{@items.length}"
+  get '/' do
+    erb :index, :locals => {:items => @items}
+  end
+
+  get "/items" do
+    "Items #{@items.length}"
+    erb :index, :locals => { :items => @items}
+  end
+
+  get "/items/new" do
+    erb :new_item
+  end
+
+  post '/form' do
+    item = params[:item]
+    @items.push(item)
+    erb :index, :locals => {:items => @items}
+  end
+
+  get "/items/:identifier" do
+    identifier = params[:identifier].to_i
+    if identifier == 42
+      return 404
+    end
+    "You found: #{@items[identifier]}"
   end
 
   run! if app_file == $0
