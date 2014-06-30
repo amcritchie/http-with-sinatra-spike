645e5d75b9ede82e4cea32c318bea7b3bdaa4ab4
Alex McRitchie <amcritchie@gmail.com>
2014-06-29 18:28:26 -0600

    commit, while the sit is working.

--- app.rb
+++ app.rb
@@ -3,37 +3,54 @@ require "sinatra/base"
 
 class MyApp < Sinatra::Application
   # register Sinatra::Reloader
-
+  @search_items = []
   def initialize
     super
-    @items = ['item 1', 'item 2', 'item 3']
+    @items = ['apple', 'anteater', 'ant']
   end
 
   get '/' do
-    erb :index, :locals => {:items => @items}
+    # @search_items = nil
+    erb :index, :locals => {:items => @items, :search_items => @search_items}
   end
 
   get "/items" do
     "Items #{@items.length}"
-    erb :index, :locals => { :items => @items}
+    erb :index, :locals => { :items => @items, :search_items => @search_items}
   end
 
   get "/items/new" do
     erb :new_item
   end
 
-  post '/form' do
+  post '/items' do
     item = params[:item]
     @items.push(item)
-    erb :index, :locals => {:items => @items}
+    erb :index, :locals  => {:items => @items, :search_items => @search_items}
+  end
+
+  post '/search' do
+    search = params[:search]
+    items = @items.select{|item| item.downcase.include? search.downcase}
+    @search_items = items.dup
+    erb :search, :locals => {:items => items, :search_items => @search_items}
+  end
+
+  get '/search' do
+    @items = params[:items]
   end
 
   get "/items/:identifier" do
-    identifier = params[:identifier].to_i
-    if identifier == 42
+    @items.length
+    @search_items
+    @identifier = params[:identifier].to_i
+    # if params[:identifier].to_i >= items.length
+    element = 2
+    if @identifier == 42
       return 404
     end
-    "You found: #{@items[identifier]}"
+    erb :item_identifier, :locals => {:item => @identifier, :element => element}
+    # erb :item_identifier, :locals => {:item => @identifier}
   end
 
   run! if app_file == $0
